d899325c83836bacba1dbb5ee6b3e683137b0c27
juiceghost <jg@renegade.se>
2021-01-04 11:21:42 +0100

    First day of React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+}, {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+}, {
+  title: 'Hackers',
+  url: 'https://hackers.org/',
+  author: 'Krillzorz',
+  num_comments: 2,
+  points: 5,
+  objectID: 2,
+}, 
+];
+
+
+const welcome = {
+  greeting: 'Yo',
+  title: 'FE20'
+};
+
+const myArray = ["I'm", "an", "array"];
+const myNumbers = [1, 4, 8];
+
+function getTitle(title) {
+  return 'From Function' + title;
+}
+
+// const map1 = array1.map(x => x * 2);
+
+function writePTags(arr) {
+  return arr.map(function (x, index) {
+    console.log(x)
+    return (<p key={index}>{x}</p>);
+  })
+}
+
+function writePTagsWithoutMap(arr) {
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(<p key={i}>{arr[i]}</p>)
+  }
+  return newArr
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Hacker Stories</h1>
+      
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+
+      <hr />
+
+      {list.map(function(item) {
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+</div> );
+})}
     </div>
   );
 }
